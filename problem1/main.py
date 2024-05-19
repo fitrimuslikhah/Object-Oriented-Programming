16d10d44e6b808892ea9f305722375ff43512ffb
fitri <fitrimuslikhah7@gmail.com>
2024-05-19 21:42:49 +0700

    problem 2

--- problem1/main.py
+++ problem1/main.py
@@ -1,1 +1,59 @@
 # tulis solusi code disini
+class Shape:
+    def __init__(self):
+        pass
+
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(self):
+        pass
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def calculate_area(self):
+        return self.side ** 2
+
+    def calculate_perimeter(self):
+        return 4 * self.side
+
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
+
+    def calculate_perimeter(self):
+        return self.base + self.height + (self.base ** 2 + self.height ** 2) ** 0.5
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+    def calculate_perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+def main():
+    shapes = {
+        "Persegi": Square(4),
+        "Segitiga": Triangle(3, 4),
+        "Persegi Panjang": Rectangle(7, 8)
+    }
+
+    for shape_name, shape in shapes.items():
+        print(f"{shape_name} :")
+        print(f"Luas : {shape.calculate_area()}")
+        print(f"Keliling : {shape.calculate_perimeter()}")
+        print()
--- problem2/main.py
+++ problem2/main.py
@@ -1,1 +1,39 @@
 # tulis solusi code disini
+import math
+
+class Kubus:
+    def __init__(self, sisi):
+        self.sisi = sisi
+
+    def volume(self):
+        return self.sisi ** 3
+
+class Balok:
+    def __init__(self, panjang, lebar, tinggi):
+        self.panjang = panjang
+        self.lebar = lebar
+        self.tinggi = tinggi
+
+    def volume(self):
+        return self.panjang * self.lebar * self.tinggi
+
+class Tabung:
+    def __init__(self, jari_jari, tinggi):
+        self.jari_jari = jari_jari
+        self.tinggi = tinggi
+
+    def volume(self):
+        return math.pi * (self.jari_jari ** 2) * self.tinggi
+
+def main():
+    kubus = Kubus(10)
+    balok = Balok(3, 6, 10)
+    tabung = Tabung(7, 10)
+
+    print(f"Volume Kubus : {kubus.volume()}")
+    print(f"Volume Balok : {balok.volume()}")
+    print(f"Volume Tabung : {tabung.volume():.0f}")
+
+if __name__ == "__main__":
+    main()
+ 
